52a0c591075ca773f4036bae9d4838291c6d513d
rafael <rafael.dorzada@student.hu.nl>
2018-05-29 23:02:59 +0200

    test uit

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class Testing {
     @Test
     public void test_fail() {
-        fail("Let's test the failing unit test");
+       // fail("Let's test the failing unit test");
     }
 }
 
